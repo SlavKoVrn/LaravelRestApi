1a077b139a8a2e703814f6febcdf1ec2369c9071
SlavKoVrn <slavko.chita@gmail.com>
2025-03-16 17:44:02 +0300

    side bar collapse

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,5 +16,18 @@
 @stop
 
 @section('js')
-    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+    <script>
+        $(function(){
+            if (localStorage.getItem('sidebar-collapse') === 'true') {
+                $('body').addClass('sidebar-collapse');
+            }
+            $('a[data-widget="pushmenu"]').click(function(){
+                if ($('body').hasClass('sidebar-collapse')) {
+                    localStorage.setItem('sidebar-collapse', false);
+                } else {
+                    localStorage.setItem('sidebar-collapse', true);
+                }
+            });
+        })
+    </script>
 @stop
