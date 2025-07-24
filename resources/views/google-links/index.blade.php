0d3d4ce40c3d07403895762abbd13c686f2b4d14
SlavKoVrn <slavko.chita@gmail.com>
2025-07-24 05:46:40 +0300

    google config on

--- resources/views/google-links/index.blade.php
+++ resources/views/google-links/index.blade.php
@@ -25,6 +25,7 @@
         <tr>
             <th>Database Table</th>
             <th>Google Link</th>
+            <th>Google Config</th>
             <th>Actions</th>
         </tr>
     </thead>
@@ -33,6 +34,7 @@
             <tr>
                 <td>{{ $link->database_table }}</td>
                 <td><a href="{{ $link->google_link }}" target="_blank">Open</a></td>
+                <td>{{ substr($link->google_config, 0, 50) }}</td>
                 <td>
                     <a href="{{ route('google-links.edit', $link) }}" class="btn btn-sm btn-warning">Edit</a>
                     <form action="{{ route('google-links.destroy', $link) }}" method="POST" style="display:inline;">
