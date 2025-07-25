feac3c86795f4aaad8f7cbf8c4ef8179a8eb9166
SlavKoVrn <slavko.chita@gmail.com>
2025-07-25 15:45:00 +0300

    import range A1:Z1001

--- app/Http/Controllers/GoogleTableController.php
+++ app/Http/Controllers/GoogleTableController.php
@@ -536,7 +536,7 @@ class GoogleTableController extends Controller
 
             // Define sheet range (adjust as needed)
             $sheetName = 'Лист1'; // Change if your sheet has a different name
-            $range = "{$sheetName}!A1:Z1000"; // Adjust range size as needed
+            $range = "{$sheetName}!A1:Z1001"; // Adjust range size as needed
 
             $values = $googleSheets->readSheet($range);
 
